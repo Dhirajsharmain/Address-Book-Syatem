8d9370830a15efe2e907c5458f100f357b81cca4
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-22 17:57:28 +0530

    Added: create a Contacts in Address Book with Basic details

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Address-Book-Syatem
+
+**UC1-CreateAContact**
+
+_Description_ : Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email…
---
+++ bridgelabz/services/AddressBook.java
@@ -0,0 +1,42 @@
+package bridgelabz.services;
+
+import bridgelabz.model.Person;
+
+import java.util.*;
+
+public class AddressBook {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Map<String ,Person> personMap = new HashMap();
+
+    /**
+     * This is the main method, Here is the starting point of the program.
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+            addPersonDetail();
+    }
+
+    /**
+     * Method for taking person details and store them into storage.
+     * In this program used hashmap.
+     */
+    private static void addPersonDetail(){
+        Person person = new Person();
+        System.out.print("Enter First Name : ");
+        person.setFirstName(scanner.nextLine());
+        System.out.print("Enter Last Name : ");
+        person.setLastName(scanner.nextLine());
+        System.out.print("Enter Address : ");
+        person.setAddress(scanner.nextLine());
+        System.out.print("Enter City : ");
+        person.setCity(scanner.nextLine());
+        System.out.print("Enter ZipCode : ");
+        person.setZip(scanner.nextLine());
+        System.out.print("Enter Phone Number : ");
+        person.setPhone(scanner.nextLine());
+
+        personMap.put(person.getFirstName(),person);
+        System.out.println(personMap.toString());
+    }
+}
